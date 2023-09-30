bc0c1c7f5180b0b6a5e97f038919ba11b82f9895
siam <samisiam851@gmail.com>
2023-09-30 22:36:14 +0600

    cart done

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 const express = require('express')
+const { ObjectId } = require('mongodb');
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
 const port = 5000
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://samisiam851:IzxHVRpaCCZiyoO9@cluster0.lkouiuy.mongodb.net/?retryWrites=true&w=majority";
+
+////////////////////////////////////////// JWT verification //////////////////////////////////////////////////
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, 'dafea91334ce03e49042a919e62de4bd212fc5d3c5c1e08656122279bb16bbadca7be7506441ff2e209f59235ab8dc4eb21ee5ae96d9816168c68e22ed9247d9', (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+
+//////////////////////////////////// CORS & body parser  Middleware//////////////////////////////////////////////////////
+
+
 
 const corsConfig = {
     origin: '*',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 }
 app.use(cors(corsConfig))
 app.options("", cors(corsConfig))
 
+
+
+
+
+app.use(bodyParser.json());
+
+
+
+/////////////////////////////////// MONGODB client creation /////////////////////////////////////////////////////////// 
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -34,13 +70,43 @@ async function run() {
 
         const database = client.db("ElectraHaven")
         const products = database.collection("products")
+        const usersCollection = database.collection("users")
+        const cart = database.collection("cart")
+
+        ///////////////////////////////////////////// USERS /////////////////////////////////////////////////
+        app.post('/users', async (req, res) => {
+            const { name, email, photoURL, role } = req.body;
+            const user = {
+                name: name,
+                email: email,
+                photoURL: photoURL,
+                role: role
+            }
+
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+
+            if (existingUser) {
+                return res.send({ message: 'user already exists', data: existingUser })
+            }
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const userData = await usersCollection.findOne({ email: email });
+            res.send(userData);
+        })
 
 
+        //////////////////////////////////////////////  PRODUCTS ////////////////////////////////////////////
 
         app.get('/products', async (req, res) => {
             let cursor = products.find();
             let result = await cursor.toArray();
-            console.log(result);
+
             res.send(result);
 
         })
@@ -49,14 +115,15 @@ async function run() {
             const query = { type: 'inverter' };
             let cursor = products.find(query);
             let result = await cursor.toArray();
-            console.log(result);
+
             res.send(result);
         })
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id
-            const query = { _id: id };
-            let result = products.findOne(query);
-            console.log(result);
+            // console.log('here id', id);
+            const query = { _id: new ObjectId(id) };
+            let result = await products.findOne(query);
+            // console.log('the cart data....................!', result);
             res.send(result);
         })
 
@@ -64,11 +131,69 @@ async function run() {
             const query = { type: 'solar panel' };
             let cursor = products.find(query);
             let result = await cursor.toArray();
-            console.log(result);
+
             res.send(result);
         })
 
+        ///////////////////////////////////////// CART /////////////////////////////////////////
 
+
+        app.post('/cart', async (req, res) => {
+            const { userEmail, productId } = req.body
+            // console.log('from cart post', userEmail, productId);
+            const query = {
+                $and: [
+                    { productId: productId },
+                    { userEmail: userEmail }
+                ]
+            }
+            let productAlreadyExists = null;
+            productAlreadyExists = await cart.findOne(query);
+            if (productAlreadyExists) {
+                res.send({ message: 'Already this product is in your cart', acknowledged: false })
+            } else {
+                const result = await cart.insertOne(req.body);
+                res.send(result);
+            }
+
+        })
+
+
+
+        app.get('/cart/:email', async (req, res) => {
+            const email = req.params.email
+            // console.log('the cart data..................................!', email);
+            const query = { userEmail: email }
+            const cursor = cart.find(query)
+            const allCartProducts = await cursor.toArray()
+            res.send(allCartProducts);
+        })
+
+        app.patch(`/cart/:id`, async (req, res) => {
+            const id = req.params.id
+            const quantity = req.body.newQuantity
+            console.log('product Id and quantity', id, quantity);
+            const query = { productId: id };
+            const cartItem = await cart.findOne(query);
+            if (!cartItem) {
+                return res.status(404).json({ message: 'Cart item not found' })
+            }
+
+            ////////////Update The Quantity////////////////////////
+            cartItem.quantity = quantity;
+
+            const result = await cart.updateOne(query, { $set: { quantity: quantity } })
+
+            res.status(200).json({ message: 'quantity updated successfully', data: result })
+        })
+
+        app.delete('/cart/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('delete API hit......!!!', id );
+            const query = { _id: new ObjectId(id) }
+            const result = await cart.deleteOne(query);
+            res.send(result);
+        })
 
 
 
